669f52ac3f79bf39906bec6931fb071c9820777d
Rubin Barclay <63505731+RubinBarclay@users.noreply.github.com>
2023-05-17 16:44:55 +0200

    finished project

---
+++ src/components/Results.vue
@@ -0,0 +1,38 @@
+<template>
+  <p>Reaction time: <span class="score">{{ score }}</span> ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+  export default {
+    props: ['score'],
+    data() {
+      return {
+        rank: null
+      }
+    },
+    mounted() {
+      if (this.score < 200) {
+        this.rank = "Ninja fingers"
+      }
+      else if (this.score < 325) {
+        this.rank = "Rapid reflexes"
+      }
+      else {
+        this.rank = "Snail pace..."
+      }
+    }
+  }
+</script>
+
+<style>
+  .score {
+    font-style: bold;
+  }
+
+  .rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+  }
+</style>
